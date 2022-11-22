6f5d59e731ead35410c24588bcd99f3328295802
Nikolai <nikdenof@gmail.com>
2022-11-22 15:37:09 +0300

    Added the cum_sum plots

--- bass_test.py
+++ bass_test.py
@@ -3,10 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-data = pd.DataFrame({'week': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-                     'revenues': [0.1, 3, 5.2, 7, 5.25, 4.9, 3, 2.4, 1.9, 1.3, 0.8, 0.6]
-                     })
-data['cum_sum'] = data['revenues'].cumsum()
 
 table_name = "SAPR.xlsx"
 table = pd.read_excel(table_name, skiprows = 2, header = None)
@@ -35,9 +31,10 @@ def c_t(x, p, q, m):
 
 
 len_table = int(table.shape[0])
-popt, pcov = curve_fit(c_t, data.cum_sum[0:11], data.revenues[1:12])
-pt_en, pc_en = curve_fit(c_t, cad_en.cum_sum[0:5], cad_en.revenues[1:6])
-popt_ru, pcov_ru = curve_fit(c_t, cad_ru.cum_sum[0:5], cad_ru.revenues[1:6], maxfev=5000)
+pt_en, pc_en = curve_fit(c_t, cad_en.cum_sum.iloc[0:5],
+                         cad_en.revenues.iloc[1:6])
+popt_ru, pcov_ru = curve_fit(c_t, cad_ru.cum_sum.iloc[0:5],
+                             cad_ru.revenues.iloc[1:6], maxfev=5000)
 
 
 def bass_model(p, q, m, T = 15):
@@ -52,7 +49,7 @@ def bass_model(p, q, m, T = 15):
         
 
 S_ru, CS_ru = bass_model(*popt_ru)
-S_en, CS_ru = bass_model(*pt_en)
+S_en, CS_en = bass_model(*pt_en)
 
 
 def rel_plot(): #delete to run
@@ -75,12 +72,29 @@ def sum_plot(): #delete to run
 
 # rev_plot but for fit data
 def fit_plot(data, S):
-    plt.plot(data['Years'], data['revenues'], label = "Реальные данные")
-    plt.plot(np.arange(len(S)), S, label = "Предсказанные данные")
+    year_start = 2016
+    year_end = 2016 + len(S)
+    plt.plot(data['Years'] + year_start - 1, data['revenues'], label = "Реальные данные")
+    plt.plot(np.arange(year_start, year_end), S, label = "Предсказанные данные")
     plt.title('Сравнение прогноза и данных')
     plt.legend(loc = 'best')
     plt.show()
 
 #Uncomment to run
-#fit_plot(cad_ru, S_ru)
-#fit_plot(cad_en, S_en)
+fit_plot(cad_ru, S_ru)
+fit_plot(cad_en, S_en)
+
+
+def cum_fit_plot(data, CS):
+    year_start = 2016
+    year_end = 2016 + len(CS)
+    plt.plot(data['Years'] + year_start - 1, data['cum_sum'], label = "Реальные данные")
+    plt.plot(np.arange(year_start, year_end), CS, label = "Предсказанные данные")
+    plt.title('Сравнение кумулитивных данных и их прогноза')
+    plt.legend(loc = 'best')
+    plt.show()
+
+
+#Uncomment to run
+cum_fit_plot(cad_ru, CS_ru)
+cum_fit_plot(cad_en, CS_en)
