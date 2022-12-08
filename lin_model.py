05e55056d419122f405b829c358deec588623658
Nikolai <nikdenof@gmail.com>
2022-12-08 19:54:02 +0300

    Added relative error and developing correct version of the fit model

--- lin_model.py
+++ lin_model.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
+# Исходные данные
+q = 40000 # Целевой показатель продаж
 # Загрузка начальных данных
 table_name = "SAPR.xlsx"
 table = pd.read_excel(table_name, skiprows = 2, header = None)
@@ -32,16 +34,48 @@ cad_sum, cs_sum = cum_sum_add(cad_sum)
 # Получение коэффициентов линейной модели спроса
 def x_t(X, a, b, y):
     x1, x2 = X # Кумулятивные продажи первого и второго продукта
-    #return a - b * (n1 - x1) + y * (n2 - x2) 
+    # return a - b * (n1 - x1) + y * (n2 - x2) 
     return a - b * x1 + y * x2 
 
 
 popt_en, _ = curve_fit(x_t, (cs_en[0:5], cs_ru[0:5]), cs_en[1:6])
+popt_ru, _ = curve_fit(x_t, (cs_ru[0:5], cs_en[0:5]), cs_ru[1:6])
 
 
-a, b, y = popt_en
+def fit_model(cs_1, cs_2, popt, t = 5):
+    cs_fit = 0
 
-plt.plot(np.arange(1, 6), x_t((cs_en[0:5], cs_ru[0:5]), *popt_en), 'r',  label = 'Fit linear model output')
-plt.scatter(np.arange(6), cs_en, label = 'Original data')
-plt.legend()
-plt.show()
+    return cs_fit
+
+
+def plt_fit(cs_1, cs_2, popt, title):
+    plt.plot(np.arange(1, 6), x_t((cs_1[0:5], cs_2[0:5]), *popt), 'r',  label = 'Fit linear model output')
+    plt.scatter(np.arange(6), cs_1, label = 'Original data')
+    plt.title(label = title)
+    plt.legend()
+    plt.show()
+
+
+plt_fit(cs_en, cs_ru, popt_en, title = "Предсказание для иностранного ПО")
+plt_fit(cs_ru, cs_en, popt_ru, title = "Предсказание для отечественного ПО")
+
+# NOT CORRECT
+#___________________________________________________________
+cs_fit_en = x_t((cs_en[0:5], cs_ru[0:5]), *popt_en)
+cs_fit_ru = x_t((cs_ru[0:5], cs_en[0:5]), *popt_ru)
+
+
+# Оценка относительной ошибки аппроксимации линейной модели
+def rae(cs_fit, cs):
+    abslte_error = np.sum(np.abs(cs - cs_fit))
+    cs_mean = np.mean(cs)
+    abslte_diff = np.sum(np.abs(cs - cs_mean))
+    rae = abslte_error / abslte_diff
+
+    return rae
+
+print("Относительная абсолютная ошибка для аппроксимации данных продаж иностанного ПО =", rae(cs_fit_en, cs_en[1:]))
+print("Относительная абсолютная ошибка для аппроксимации данных продаж отечественного ПО =", rae(cs_fit_ru, cs_ru[1:]))
+
+
+# Предсказание линейной модели без субсидии
